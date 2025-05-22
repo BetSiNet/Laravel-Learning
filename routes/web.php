ce995e657bbf4e155740796acc3233d27d95dd1f
BetSiNet <bethsineth7898@gmail.com>
2025-05-23 02:00:53 +0700

    Routing Ninjas Page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    // view is the folder , welcome is name of file
     return view('welcome');
+});
+
+Route::get('/ninjas',function(){
+    return view('ninjas.index');
 });
